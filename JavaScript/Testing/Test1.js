43e5e996f4ac01e3b4e1d534e30c9ac9587b6a92
Name <hillowoalde@gmail.com>
2022-07-22 09:42:50 -0400

    Import All Files
    All Files have now been imported

---
+++ JavaScript/Testing/Test1.js
@@ -0,0 +1,13 @@
+1+1
+10-7
+2*3
+24/6
+8/1+3
+8/(1+3)
+let noOfStudent = 20
+let noOfStudent = 0.2
+let noOfStudent = 20
+let PizzaPerStudent = 0.2
+let pizzasNeeded = noOfStudent * PizzaPerStudent
+pizzasNeeded
+
---
+++ JavaScript/Website/Any other Challenge/Test1.js
@@ -0,0 +1,4 @@
+function Luck(){
+    let number = Math.floor(Math.random()*100);
+    console.log (number);
+}
