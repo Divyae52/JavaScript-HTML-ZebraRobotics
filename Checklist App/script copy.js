4e0f40fd79d85f364743f2fbc5c04871c16f5ef4
Name <hillowoalde@gmail.com>
2022-08-03 09:33:25 -0400

    Continued Working
    Meant to save yesterday

--- Checklist App/script copy.js
+++ Checklist App/script copy.js
@@ -1,19 +1,17 @@
 hi = "hello"
-let list = [
-    {name: "test", checked: false },
-    {name: "bobby", checked: false },
-    {name: "aiufenv ay-u-fin-ev", checked: false } 
-];
-
+list = [
+    { name: "test", checked: false }
+]
 
 function render_list() {
     // this function will render the shopping list in the DOM
     // vender the shopping list in the DoM
     console.log('`render_list` ran')
-    const shopping_list_string = generate_list_string(list)
+    const shopping_list_string = generate_list_string
+    const shoppingListItemsString = ['<li>'+ generate_item_element("a") +'</li>']
     // insert that HTML into the DOM
 
-    $('.js-shopping-list').html(shopping_list_string);
+    $('.js-shopping-list').html(shoppingListItemsString);
 
 }
 
@@ -51,14 +49,13 @@ function handle_shopping_list() {
 }
 function generate_list_string(shopping_list) {
     console.log("Generating shopping list element")
-    let htmlThing = " ";
-    for (let i = 0; i<shopping_list.length; i++) {
-        console.log(shopping_list[i])
-        htmlThing += generate_item_element(shopping_list[i]);
-    }
-    console.log(htmlThing)
-    return htmlThing;
-    }
+    return `
+        <li>apples</li>
+        <li>oranges</li>
+        <li>milk</li>
+        <li>bread</li>
+        `
+}
 
 function generate_item_element(item) {
     return `
@@ -76,26 +73,6 @@ function generate_item_element(item) {
         </li>`;
 }
 
-function add_item_to_list(itemName){
-    console.log(`Adding "${itemName}" to shopping list`) ;
-    // add an item to the CHECKLIST
 
-
-}
-    function handle_add_item() {
-        $('#js-shopping-list-form').submit(function(event) {
-        event.preventDefault();
-        console.log('`handle_add_item` ran');
-        //get the value in the form input element
-        //reset the value to ""
-        // add the value to list (use above empty function)
-        //render list (use existing function)
-        var reciptCost=document.getElementById("cost")
-        item = reciptCost.innerHTML
-        reciptCost.innerHTML = ""
-        list.push({name: ""})
-        })
-    }
-    
 //  when the page loads, call ~handleShoppingList™
 (handle_shopping_list());
